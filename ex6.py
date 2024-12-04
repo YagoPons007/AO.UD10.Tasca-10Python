5c3ee6a19d021479a3ea70acbd525854e7256bc6
YagoPons007 <ypons20937@iesjoanramis.org>
2024-12-04 22:00:54 +0900

    Exercicis TASCA 10

---
+++ ex6.py
@@ -0,0 +1,18 @@
+def gran_de_tres():
+        if num1 > num2:
+            if num1 > num3:
+                print(f"{num1} és el major")
+        elif num2 > num1: 
+            if num2 > num3:
+                print(f"{num2} és el major")
+        elif num3 > num1 
+            if num3 > num2:
+                print(f"{num3} és el major")
+
+while True:
+
+    print("Introdueix tres nombre enters diferentes: ")
+    num1 = int(input("Nombre 1: "))
+    num2 = int(input("Nombre 2: "))
+    num3 = int(input("Nombre 3: "))
+    gran_de_tres()
